baf5df778764df8360c0b1819e02bf32d7fc61aa
Lara Bjork <lara.m.bjork@gmail.com>
2019-10-25 09:55:05 -0700

    add var definitions to jQuery

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,12 @@ $(document).ready(function (){
   event.preventDefault;
 
 
+  var question1 = $("input:radio[name=q1]:checked").val();
+  var question2 = $("input:radio[name=q2]:checked").val();
+  var question3 = $("input:radio[name=q3]:checked").val();
+  var question4 = $("input:radio[name=q4]:checked").val();
+  var question5 = $("input:radio[name=q5]:checked").val();
+
 
 
 
