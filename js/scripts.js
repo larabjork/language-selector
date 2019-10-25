2791fa694463674998d35e964fbb5afb1cc251e7
Lara Bjork <lara.m.bjork@gmail.com>
2019-10-25 10:16:01 -0700

    add first try at if-else-else if statement

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 $(document).ready(function (){
-  $("form#quiz").submit(function(event))
-  event.preventDefault;
+  $("form#quiz").submit(function(event)
+  {
+    event.preventDefault();
 
 
   var question1 = $("input:radio[name=q1]:checked").val();
@@ -9,9 +10,25 @@ $(document).ready(function (){
   var question4 = $("input:radio[name=q4]:checked").val();
   var question5 = $("input:radio[name=q5]:checked").val();
 
+  if (question1 === "Elegant layout; aesthetics are really important to me.") {
+          $(".answer").show();
+          $("#python").show();
+          $("#sql").hide();
+          $("#scala").hide();
+          $("#unclear").hide();
+        // } else if (selection === "Snakes"){
+        //   $(".snake").show();
+        //   $(".turtle").hide();
+        //   $(".insect").hide();
+        } else {
+          $(".answer").show();
+          $("#python").hide();
+          $("#sql").show();
+          $("#scala").show();
+          $("#unclear").hide();
+        }
 
-
-
+});
 
 
 
